502afcdb576d55444889344ff0cfac4027c2d6b0
EisenHao <ei13911468370@gmail.com>
2019-03-02 10:34:48 +0800

    20190302

--- No53.maximum-subarray.cpp
+++ No53.maximum-subarray.cpp
@@ -1,3 +1,22 @@
+/** 题目描述：
+ * 53. 最大子序和
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+示例:
+输入: [-2,1,-3,4,-1,2,1,-5,4],
+输出: 6
+解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+
+ *
+ * Encoding：utf-8
+ * Programming language：c++
+ * Coder：eisenhao
+ * 20190228
+ * */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
@@ -21,3 +40,14 @@ public:
         return result;
     }
 };
+
+int main(int argc, const char * argv[]) {
+    Solution solution;
+    vector<int> Input = {-2,1,-3,4,-1,2,1,-5,4};
+    cout << "Input = {";
+    for(int i=0; i<Input.size()-1; i++){
+        cout << Input[i] << ",";
+    }
+    cout << Input[Input.size()-1] <<"}" << endl;
+    cout << "Output = " << solution.maxSubArray(Input)  << endl;
+}
