40b46232970789fff1c2ef20f5a083de50709502
EisenHao <ei13911468370@gmail.com>
2020-10-04 10:47:53 +0800

    No2. 两数相加

--- No2.add-two-numbers.cpp
+++ No2.add-two-numbers.cpp
@@ -1,41 +1,34 @@
-#include <iostream>
-#include <vector>
-using namespace std;
 /**
- * 2. 两数相加
+ * No2. 两数相加
  * 给出两个非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照逆序的方式存储的，并且它们的每个节点只能存储一位数字。
-
-如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
-
-您可以假设除了数字 0 之外，这两个数都不会以 0开头。
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0开头。
 
 示例：
-
 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
 输出：7 -> 0 -> 8
 原因：342 + 465 = 807
 */
 
-//Definition for singly-linked list.
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
+#include <iostream>
+#include <vector>
+#include "myTools.h"
+using namespace std;
 
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *ptrL1 = l1, *ptrL2 = l2, *tmpListNode = NULL;
+        ListNode *ptrL1 = l1, *ptrL2 = l2;
         ListNode *ptrAns = new ListNode(0), *ansHead = ptrAns;// 生成临时头节点
         int jinWei = 0, tmp = 0;
 
         while (ptrL1 != NULL && ptrL2 != NULL) {
             tmp = ptrL1->val + ptrL2->val + jinWei;
-            jinWei = 0;
             if (tmp > 9) {
                 tmp -= 10;
                 jinWei = 1;
+            } else {
+                jinWei = 0;
             }
             ptrAns->next = new ListNode(tmp);
             ptrAns = ptrAns->next;
@@ -48,10 +41,11 @@ public:
         }
         while (ptrL1 != NULL) {
             tmp = ptrL1->val + jinWei;
-            jinWei = 0;
             if (tmp > 9) {
                 tmp -= 10;
                 jinWei = 1;
+            } else {
+                jinWei = 0;
             }
             ptrAns->next = new ListNode(tmp);
             ptrAns = ptrAns->next;
@@ -61,7 +55,6 @@ public:
         // 进位处理
         if (jinWei == 1) {
             ptrAns->next = new ListNode(1);
-            ptrAns = ptrAns->next;
         }
 
         // 去掉临时头
@@ -74,18 +67,11 @@ public:
 
 int main() {
     Solution solution;
-
-    ListNode *ptrL1 = new ListNode(9);
-    ptrL1->next = new ListNode(9);
-    ListNode *ptrL2 = new ListNode(1);
-    ptrL2->next = new ListNode(3);
-
+    ListNode *ptrL1 = new ListNode(3);
+    ptrL1->next = new ListNode(2);
+    ListNode *ptrL2 = new ListNode(5);
+    ptrL2->next = new ListNode(4);
     ListNode *result = solution.addTwoNumbers(ptrL1, ptrL2);
-
-    while (result != NULL) {
-        cout << result->val << endl;
-        result = result->next;
-    }
-
+    MyTools::printList(result);
     return 0;
 }
