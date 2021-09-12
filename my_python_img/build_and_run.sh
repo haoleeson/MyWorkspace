661202044969e8318201e0d5f68aa02ef46d009b
EisenHao <ei13911468370@gmail.com>
2021-09-12 10:20:13 +0800

    Add never stop program.

--- my_python_img/build_and_run.sh
+++ my_python_img/build_and_run.sh
@@ -12,23 +12,25 @@ CONTAINER_NAME=my-python-container
 SHARED_DIR=/c/Workspace/MyDockers/commonShareDir
 # User Config End
 
-# Remove old container and images(if exist)
+echo "Remove the old same name container (if exist)"
 ret=$(docker ps -a | grep $CONTAINER_NAME)
 if [ -n "$ret" ]; then
     docker container stop $CONTAINER_NAME
     docker rm -f $CONTAINER_NAME
 fi
 
+echo "Remove the old same name images (if exist)"
 ret=$(docker images | grep $IMAGE_NAME)
 if [ -n "$ret" ]; then
     docker rmi -f $IMAGE_NAME
 fi
 
-# Build Python image
+echo "Build Python image"
 docker build -t $IMAGE_NAME:$IMAGE_TAG .
 
-# Run the image in a container
+echo "Run the image in a container"
 docker run -dit -p 4000:80 \
         --name $CONTAINER_NAME \
-        $IMAGE_NAME:$IMAGE_TAG \
-        /bin/sh -c "while true;do echo hello docker;sleep 1;done"
+        $IMAGE_NAME:$IMAGE_TAG
+
+echo "done."
