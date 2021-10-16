063852357e915002abd93dc0d4fe4a3ba6b7f9b7
EisenHao <ei13911468370@gmail.com>
2021-10-17 00:37:05 +0800

    add Sed_replace_in_file.sh

---
+++ Sed_replace_in_file.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+User='user1'
+OldPwd='abc'
+NewPwd='def'
+FilePath='/Users/lihao52/code/Python/a.txt'
+
+echo ${User}
+echo ${OldPwd}
+echo ${NewPwd}
+echo "sed -i '' 's:'${User}'\:'${OldPwd}':'${NewPwd}':g' ${FilePath}"
+
+sed -i '' 's:'${User}'\:'${OldPwd}':'${User}'\:'${NewPwd}':g' ${FilePath}
