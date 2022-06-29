07646df8c7d27350bd66847fbb19ab1115a4bf23
EisenHao <ei13911468370@gmail.com>
2022-06-29 19:41:25 +0800

    update dijkstra coding

--- MyCpp/5_Dijkstra/DenseMatrixGraph.h
+++ MyCpp/5_Dijkstra/DenseMatrixGraph.h
@@ -6,152 +6,185 @@
 #ifndef _DENSEMATRIXGRAPH_H
 #define _DENSEMATRIXGRAPH_H
 #include <cassert>
+#include <iomanip>
 #include <iostream>
 #include <vector>
-#include <iomanip>
 
-#define WuQiongDa 999
 using namespace std;
 
-class DenseMatrixGraph{//构建稠密图类，使用邻接矩阵法表示
+#define MAX_VAL 999
+
+// 构建稠密图类，使用邻接矩阵法表示
+class DenseMatrixGraph {
 private:
-    int n;//n为图的顶点数量
-    int m;//m为边的数量
-    bool directed;//是否为有向图
-    /**
-     * 构建二维邻接矩阵g，其存储各边的权值<int>, 默认值为无穷大WuQiongDa，即两节点无边
-     * */
-    vector<vector<int>> g;
+    int n;                 // n为图的顶点数量
+    int m;                 // m为边的数量
+    bool directed;         // 是否为有向图
+    vector<vector<int>> g; // 二维邻接矩阵，存储各边的权值<int>
+
 public:
-    /**构造函数
-     * 定义图的顶点数，生成并初始化(顶点数 x 顶点数)规模的二维邻接矩阵g
-     * 此时没插入边的信息，所有边的权值均为：（默认值）无穷大WuQiongDa
-     * 传入参数：*/
-    DenseMatrixGraph(int n, bool directed){
-        this->n = n;//顶点数
-        this->directed = directed;
-        this->m = 0; //边数为0
-        //初始化邻接矩阵的关系全部为WuQiongDa，即各节点之间都不连接
-        for(int i=0; i<n; i++){
-            g.push_back(vector<int>(n, WuQiongDa));
-        }
-        //节点自己到自己为0
-        for(int i=0; i<n; i++){
-            g[i][i] = 0;
-        }
-    }
+    // 构造函数
+    DenseMatrixGraph(int n, bool directed);
 
-    //析构函数
-    ~DenseMatrixGraph(){
-    }
+    // 析构函数
+    ~DenseMatrixGraph() {}
 
-    //返回图中的顶点数量
-    int getV(){
-        return this->n;
-    }
-    //返回图中的边数量
-    int getE(){
-        return this->m;
-    }
+    // 返回图中的顶点数量
+    int getV() { return this->n; }
 
-    //判断节点v到节点w之间是否已经存在边
-    bool hasEdge(int v, int w){
-        bool result = false;
-        assert(v>=0 && v<n);//防止越界
-        assert(w>=0 && w<n);
-        if(g[v][w] < WuQiongDa){
-            result = true;//有边
-        }
-        return result;
-    }
+    // 返回图中的边数量
+    int getE() { return this->m; }
 
-    /**在v节点和w节点之间建立连接关系(边)
-     * 参数：节点v, 节点w, 两节点之间的权值
-     * */
-    void addEdge(int v, int w, int value){
-        assert(v>=0 && v<n);
-        assert(w>=0 && w<n);//防止数组越界访问
-        g[v][w]= value;
-        m++;//增加边的数量
-        //如果为有向图，只增加这一条边
-        if(directed){
-            return;
-        }
-        //如果为无向图,则邻接图成对称关系，w节点到v节点之间也一定是存在连接的
-        else{
-            g[w][v]= value;
-        }
-    }
+    // 判断节点 v 到节点 w 之间是否已经存在边
+    bool hasEdge(int v, int w);
 
-    //显示邻接矩阵
-    void showMatrixGraph(){
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                cout <<setw(6)<< g[i][j];
-            }
-            cout << endl;
-        }
-    }
+    // 添加边
+    void addEdge(int v, int w, int value);
+
+    // 显示节点路径
+    void printPath(int v, int w, vector<int>& prePath);
+
+    // 显示邻接矩阵
+    void showMatrixGraph();
 
     /**
      * Dijkstra算法实现
      * 参数：起始节点 v， 终止节点 w
      * */
-    int Dijkstra(int v, int w){
-        /** 构建并初始化一维矩阵dist距离
-         * 表示：起始节点v 到其他所有节点的距离
-         * */
-        vector<int> dist;
-        for(int i=0; i<this->n; i++){
-            dist.push_back(WuQiongDa);
-        }
-        dist[v] = 0;//节点到自身的距离为0
-        //构建存储中间前驱节点变量的一维
-        vector<int> path;
-        for(int i=0; i<this->n; i++){
-            path.push_back(-1);
-        }
-        path[v] = v;// 起始节点的前驱节点为其自身
-
-        //每个结点都尝试做中间节点 k
-        for(int k=0; k<this->n; k++){
-            //临时的目的节点(j)： 从v到j： v->k、k->j
-            for(int j=0; j<this->n; j++){
-                if(dist[k]!=WuQiongDa && g[k][j]!=WuQiongDa && (dist[j] > dist[k]+g[k][j])){
-                    dist[j] = dist[k]+g[k][j];//更新 v到j 距离
-                    path[j] = k;// 记录前驱节点 k
-                }
-            }
-        }
-        //输出起始节点 v 到所有节点的 最短距离
-//        cout << "dist = " << endl;
-//        for(int i=0; i<this->n; i++){
-//            cout << setw(6) << " 0->"<< i << "=" << dist[i];
-//        }
-//        cout << endl;
-        //输出最短距离下每个节点的前驱节点
-//        for(int i=0; i<this->n; i++){
-//            cout << setw(6) << "Pre" << i << "=" << path[i];
-//        }
-//        cout << endl;
-        cout << "The shortest path Length of ("<< v << "->" << w << ") = " << dist[w] << endl;
-        cout << "Path: ";
-        int from = this->n -1;
-        vector<int> OutPath;
-        OutPath.push_back(w);
-        while(path[from]!=v){
-            OutPath.push_back(path[from]);
-            from = path[from];
-        }
-        OutPath.push_back(v);
-        for(int i=OutPath.size()-1; i>=0; i--){
-            cout << OutPath[i];
-            if(i!=0){
-                cout << " -> ";
-            }
-        }
-        return dist[w];
-    }
+    int Dijkstra(int v, int w);
 };
+
+/**
+ * 构造函数
+ * @param n 图的顶点数
+ * @param directed 是否为有向图
+ */
+DenseMatrixGraph::DenseMatrixGraph(int n, bool directed) : n(n), m(0), directed(directed) {
+    // 初始化邻接矩阵的关系全部为无穷大，即各节点之间都不连接
+    for (int i = 0; i < n; i++) {
+        g.push_back(vector<int>(n, MAX_VAL));
+    }
+
+    // 节点自己到自己距离为 0
+    for (int i = 0; i < n; i++) {
+        g[i][i] = 0;
+    }
+}
+
+/**
+ * 判断两节点之间是否有边
+ * @param v 节点 v
+ * @param w 节点 w
+ * @return
+ */
+bool DenseMatrixGraph::hasEdge(int v, int w) {
+    // 断言，防止越界
+    assert(v >= 0 && v < n && w >= 0 && w < n);
+
+    return g[v][w] < MAX_VAL;
+}
+
+/**
+ * 连接两节点，在两节点之间添加边
+ * @param v 节点 v
+ * @param w 节点 w
+ * @param value 权重
+ */
+void DenseMatrixGraph::addEdge(int v, int w, int value) {
+    // 断言，防止越界
+    assert(v >= 0 && v < n && w >= 0 && w < n);
+
+    // 若为有向图，只增加这一条边；若为无向图，对称增加权重
+    if (directed) {
+        g[v][w] = value;
+    } else {
+        g[v][w] = value;
+        g[w][v] = value;
+    }
+
+    // 增加边的数量
+    ++m;
+}
+
+/**
+ * 显示邻接矩阵
+ */
+void DenseMatrixGraph::showMatrixGraph() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << setw(6) << g[i][j];
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+}
+
+/**
+ * 显示最短途经路径
+ * @param v 节点 v
+ * @param w 节点 w
+ * @param prePath 各节点的前置路径
+ */
+void DenseMatrixGraph::printPath(int v, int w, vector<int>& prePath) {
+    cout << "The shortest path: " << endl;
+
+    // 最短路径
+    vector<int> shortestPath;
+    // 填充目标节点
+    shortestPath.push_back(w);
+
+    // 逆序遍历前置路径 prePath，得到逆序最短路径
+    for (int from = this->n - 1; prePath[from] != v; from = prePath[from]) {
+        shortestPath.push_back(prePath[from]);
+    }
+
+    // 填充起始节点
+    shortestPath.push_back(v);
+
+    // 逆序输出，得到正向最短路径
+    for (int i = shortestPath.size() - 1; i >= 0; i--) {
+        cout << shortestPath[i];
+        if (i != 0) {
+            cout << " -> ";
+        }
+    }
+
+    cout << endl;
+}
+
+/**
+ * Dijkstra 算法实现，返回节点 v 到节点 w 的距离
+ * @param v 起始节点 v
+ * @param w 终止节点 w
+ * @return -1 -- 不可达；dist -- 最短距离
+ */
+int DenseMatrixGraph::Dijkstra(int v, int w) {
+    // 节点 v 到其他所有节点的距离
+    vector<int> dist(this->n, MAX_VAL);
+    dist[v] = 0; // 节点到自身的距离为0
+
+    // 记录节点 v 到节点 w 的最短路径
+    vector<int> prePath(this->n, -1);
+    prePath[v] = v; // 起始节点的前驱节点为其自身
+
+    // 每个结点都尝试做中间节点 k
+    for (int k = 0; k < this->n; k++) {
+        // 临时的目的节点(j)： 从 v 到 j： v->k、k->j
+        for (int j = 0; j < this->n; j++) {
+            // 若从 v -> k -> j 距离更短，则更新 v -> j 距离
+            if (dist[k] != MAX_VAL && g[k][j] != MAX_VAL && (dist[j] > dist[k] + g[k][j])) {
+                dist[j] = dist[k] + g[k][j];
+                // 记录前驱节点 k
+                prePath[j] = k;
+            }
+        }
+    }
+
+    // 打印途径的节点路径
+    printPath(v, w, prePath);
+
+    return dist[w];
+}
 
 #endif
--- MyCpp/5_Dijkstra/DijkstraMain.cpp
+++ MyCpp/5_Dijkstra/DijkstraMain.cpp
@@ -5,10 +5,28 @@
  * */
 #include "DenseMatrixGraph.h"
 
-int main(int argc, const char * argv[]) {
-    //定义一个稠密图实例
-    DenseMatrixGraph *graph = new DenseMatrixGraph(16, false);//顶点数：16，有向图：false
+// 添加各个边（构造图）
+void addEdges(DenseMatrixGraph* graph);
+
+int main(int argc, const char* argv[]) {
+    // 初始化一个无向图对象
+    DenseMatrixGraph* graph = new DenseMatrixGraph(16, false);
+
     //添加边的信息
+    addEdges(graph);
+
+    //显示邻接矩阵
+    graph->showMatrixGraph();
+
+    // 计算 节点0 -> 节点15 的最短路径
+    int shortestDistance = graph->Dijkstra(0, 15);
+    cout << endl << "The distance of the shortest path of (0->15) = " << shortestDistance << endl;
+
+    return 0;
+}
+
+// 添加各个边（构造图）
+void addEdges(DenseMatrixGraph* graph) {
     graph->addEdge(0, 1, 5);
     graph->addEdge(0, 2, 3);
     graph->addEdge(1, 3, 1);
@@ -39,8 +57,4 @@ int main(int argc, const char * argv[]) {
     graph->addEdge(12, 14, 6);
     graph->addEdge(13, 15, 4);
     graph->addEdge(14, 15, 3);
-
-    int shorstedtDistance = graph->Dijkstra(0, 15);
-//    graph->showMatrixGraph(); //显示邻接矩阵
-    return 0;
 }
