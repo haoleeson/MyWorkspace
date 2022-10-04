4815ed3dcbe143f2bcaec10d465d0ca496c85abe
EisenHao <ei13911468370@gmail.com>
2022-10-04 21:08:12 +0800

    Add a script to start wls with static ip on Windows

---
+++ MyDocker/start_wls2_with_fixed_ip.bat
@@ -0,0 +1,33 @@
+@echo off
+setlocal enabledelayedexpansion
+
+setLocal
+:: set template var
+set wslSysVersion=Ubuntu-20.04
+set IpPrefix=172.24.32
+set wslHostIp=172.24.32.181
+set vEthernetWslIp=172.24.32.1
+
+:: Start wsl with target IP
+wsl --shutdown %wslSysVersion%
+:: start ssh
+wsl -u root -d %wslSysVersion% service ssh start
+:: start docker
+wsl -u root -d %wslSysVersion% service docker start
+echo wsl -u root ip addr add %wslHostIp%/24 broadcast %IpPrefix%.0 dev eth0 label eth0:1
+wsl -u root ip addr add %wslHostIp%/24 broadcast %IpPrefix%.0 dev eth0 label eth0:1
+echo set WSL2 %wslSysVersion% ip success: %wslHostIp%
+
+:: Add sub net of vEthernet with same IP prefix
+ipconfig | findstr %vEthernetWslIp% > nul
+if !errorlevel! equ 0 (
+    echo windows ip has been setted %vEthernetWslIp%
+) else (
+    echo netsh interface ip add address "vEthernet (WSL)" %vEthernetWslIp% 255.255.255.0
+    netsh interface ip add address "vEthernet (WSL)" %vEthernetWslIp% 255.255.255.0
+    echo set windows ip success: %vEthernetWslIp%
+)
+
+endlocal
+
+pause
