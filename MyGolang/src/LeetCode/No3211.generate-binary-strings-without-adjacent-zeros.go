80334fcffd357788feff5aded84d85e56c67e23f
haoleeson <haoleeson@gmail.com>
2024-10-29 22:39:50 -0500

    coding(LeetCode): No3211.generate-binary-strings-without-adjacent-zeros

---
+++ MyGolang/src/LeetCode/No3211.generate-binary-strings-without-adjacent-zeros.go
@@ -0,0 +1,25 @@
+/*
+No3211.generate-binary-strings-without-adjacent-zeros
+*/
+
+func validStrings(n int) []string {
+	tmp := make([]string, 2)
+	tmp[0] = "0"
+	tmp[1] = "1"
+	var l int = 0
+
+	for i := 2; i <= n; i++ {
+		l = i - 2
+		tmpAdd := make([]string, 0)
+		for _, s := range tmp {
+			// try add "0"
+			if s[l] != '0' {
+				tmpAdd = append(tmpAdd, s+"0")
+			}
+			// try add "1"
+			tmpAdd = append(tmpAdd, s+"1")
+		}
+		tmp = tmpAdd
+	}
+	return tmp
+}
