093fcd018c611a8f4296fcf2f1f8d5143ac97124
haoleeson <haoleeson@gmail.com>
2025-09-28 01:18:12 +0000

    coding(LeetCode): No139.word-break

--- MyGolang/src/LeetCode/No139.word-break.go
+++ MyGolang/src/LeetCode/No139.word-break.go
@@ -2,71 +2,61 @@
 No139.word-break
 */
 
-func cmpString(s string, startIdx int, word string) bool {
-	n := len(s)
-	m := len(word)
-	if startIdx+m > n {
-		return false
-	}
-	for i := 0; i < m; i++ {
-		if s[startIdx+i] != word[i] {
-			return false
-		}
-	}
-	return true
-}
-
-func dps(s string, wordDict []string, startIdx int, chooseWord string) bool {
-	n := len(s)
-	if startIdx == n {
-		return true
-	}
-
-	// this choose
-	if len(chooseWord) > 0 {
-		if cmpString(s, startIdx, chooseWord) {
-			startIdx += len(chooseWord)
-			if startIdx == n {
-				return true
-			}
-		} else {
-			return false
-		}
-	}
-
-	// next choose
-	for _, word := range wordDict {
-		if dps(s, wordDict, startIdx, word) {
-			return true
-		}
-	}
-	return false
-}
-
-// 深度优先搜索（超时）
-func wordBreak0(s string, wordDict []string) bool {
-	return dps(s, wordDict, 0, "")
-}
-
-// 动态规划
+// dp
 func wordBreak(s string, wordDict []string) bool {
-	wordDictSet := make(map[string]bool)
-	for _, w := range wordDict {
-		wordDictSet[w] = true
+	n := len(s)
+	wordDictMap := make(map[string]bool)
+	for _, word := range wordDict {
+		wordDictMap[word] = true
 	}
 
-	dp := make([]bool, len(s)+1)
+	dp := make([]bool, n+1)
 	dp[0] = true
 
-	// dp[i] = dp[j] && s[j:i] in wordDict
-	for i := 1; i <= len(s); i++ {
+	for i := 1; i <= n; i++ {
 		for j := 0; j < i; j++ {
-			if dp[j] && wordDictSet[s[j:i]] {
+			if dp[j] && wordDictMap[s[j:i]] {
 				dp[i] = true
 				break
 			}
 		}
 	}
+	return dp[n]
+}
 
-	return dp[len(s)]
+// dfs 超时
+func wordBreak0(s string, wordDict []string) bool {
+	var spillAble bool = false
+
+	var startEqual func(string, string) bool
+	startEqual = func(cmpStr, cmpWord string) bool {
+		lenW := len(cmpWord)
+		if len(cmpStr) < lenW {
+			return false
+		}
+		for chIdx := 0; chIdx < lenW; chIdx++ {
+			if cmpStr[chIdx] != cmpWord[chIdx] {
+				return false
+			}
+		}
+		return true
+	}
+
+	var dfs func(string)
+	dfs = func(str string) {
+		if len(str) == 0 {
+			spillAble = true
+			return
+		}
+
+		for _, word := range wordDict {
+			if startEqual(str, word) {
+				dfs(str[len(word):])
+			}
+		}
+	}
+
+	dfs(s)
+
+	return spillAble
 }
