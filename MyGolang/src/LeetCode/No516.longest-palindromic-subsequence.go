636ff7310c91fdde5ff8844fc514d1ff64af6d10
haoleeson <haoleeson@gmail.com>
2025-11-17 00:55:49 +0000

    coding(LeetCode): No516.longest-palindromic-subsequence

---
+++ MyGolang/src/LeetCode/No516.longest-palindromic-subsequence.go
@@ -0,0 +1,31 @@
+/*
+No516.longest-palindromic-subsequence
+*/
+
+func longestPalindromeSubseq(s string) int {
+	n := len(s)
+	dp := make([][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int, n)
+		dp[i][i] = 1
+	}
+
+	var max func(int, int) int
+	max = func(x, y int) int {
+		if y > x {
+			return y
+		}
+		return x
+	}
+
+	for i := n - 1; i >= 0; i-- {
+		for j := i + 1; j < n; j++ {
+			if s[i] == s[j] {
+				dp[i][j] = dp[i+1][j-1] + 2
+			} else {
+				dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[0][n-1]
+}
