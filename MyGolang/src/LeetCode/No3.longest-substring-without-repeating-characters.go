080bd2fd8979746daddce2b60b480f01a0b81589
haoleeson <haoleeson@gmail.com>
2025-11-23 02:38:41 +0000

    coding(LeetCode): No3.longest-substring-without-repeating-characters

--- MyGolang/src/LeetCode/No3.longest-substring-without-repeating-characters.go
+++ MyGolang/src/LeetCode/No3.longest-substring-without-repeating-characters.go
@@ -31,3 +31,30 @@ func lengthOfLongestSubstring(s string) int {
 
 	return ans
 }
+
+func lengthOfLongestSubstring2(s string) int {
+	n := len(s)
+	maxSubLen := 0
+	chMap := make(map[byte]bool)
+
+	// split window
+	for l, r := 0, 0; r < n; r++ {
+		ch := s[r]
+		if !chMap[ch] {
+			// not dumplicate
+			chMap[ch] = true
+			if r-l+1 > maxSubLen {
+				maxSubLen = r - l + 1
+			}
+		} else {
+			// dumplicate
+			// move left to the dumplicate ch idx
+			for ; s[l] != ch; l++ {
+				chMap[s[l]] = false
+			}
+			// move left to behind the dumplicate ch idx
+			l++
+		}
+	}
+	return maxSubLen
+}
