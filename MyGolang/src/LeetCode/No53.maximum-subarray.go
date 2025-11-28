b27932e93c62d99fcb82d2c68c3cf6922b6f2409
haoleeson <haoleeson@gmail.com>
2025-11-28 00:51:58 +0000

    Recoding(LeetCode): No53.maximum-subarray

--- MyGolang/src/LeetCode/No53.maximum-subarray.go
+++ MyGolang/src/LeetCode/No53.maximum-subarray.go
@@ -1,7 +1,33 @@
 /*
 No53.maximum-subarray
 */
+// 2025-11-28 recoding
 func maxSubArray(nums []int) int {
+	n := len(nums)
+	maxSubSum := nums[0]
+	var dp_i, dp_i_1 int = 0, nums[0]
+
+	var max func(int, int) int
+	max = func(x, y int) int {
+		if y > x {
+			return y
+		}
+		return x
+	}
+
+	for i := 1; i < n; i++ {
+		dp_i = max(dp_i_1+nums[i], nums[i])
+		if dp_i > maxSubSum {
+			maxSubSum = dp_i
+		}
+
+		dp_i_1 = dp_i
+	}
+
+	return maxSubSum
+}
+
+func maxSubArray0(nums []int) int {
 	dp_i, dp_i_1, ret := 0, 0, math.MinInt
 
 	for _, num := range nums {
