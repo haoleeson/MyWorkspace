bd51425e6608a061cd25cda5c7ed21a99540233f
haoleeson <haoleeson@gmail.com>
2025-12-02 01:17:42 +0000

    Recoding(LeetCode): No200.number-of-islands

--- MyGolang/src/LeetCode/No200.number-of-islands.go
+++ MyGolang/src/LeetCode/No200.number-of-islands.go
@@ -1,6 +1,53 @@
 /*
 No200.number-of-islands
 */
+
+// 2025-12-02 recoding
+func numIslands(grid [][]byte) int {
+	m := len(grid)
+	n := len(grid[0])
+
+	visFlag := make([][]bool, m)
+	for i := 0; i < m; i++ {
+		visFlag[i] = make([]bool, n)
+	}
+
+	var bfs func(int, int)
+	bfs = func(x, y int) {
+		if x < 0 || x >= m || y < 0 || y >= n {
+			return
+		}
+		if grid[x][y] != '1' {
+			return
+		}
+		if visFlag[x][y] {
+			return
+		}
+		// flag it
+		visFlag[x][y] = true
+		bfs(x-1, y)
+		bfs(x+1, y)
+		bfs(x, y-1)
+		bfs(x, y+1)
+	}
+
+	var landCnt int = 0
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if visFlag[i][j] {
+				continue
+			}
+			if grid[i][j] != '1' {
+				continue
+			}
+			landCnt++
+			bfs(i, j)
+		}
+	}
+	return landCnt
+}
+
+
 func bfs(grid [][]byte, visitFlag [][]bool, x, y, m, n int) {
 	visitFlag[x][y] = true
 	// up
@@ -21,7 +68,7 @@ func bfs(grid [][]byte, visitFlag [][]bool, x, y, m, n int) {
 	}
 }
 
-func numIslands(grid [][]byte) int {
+func numIslands0(grid [][]byte) int {
 	m := len(grid)
 	n := len(grid[0])
 
