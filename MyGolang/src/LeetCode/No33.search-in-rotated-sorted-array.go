6d49ea45378ac9521649453d1888a13b5f9c6825
haoleeson <haoleeson@gmail.com>
2025-12-03 01:13:14 +0000

    Recoding(LeetCode): No33.search-in-rotated-sorted-array

--- MyGolang/src/LeetCode/No33.search-in-rotated-sorted-array.go
+++ MyGolang/src/LeetCode/No33.search-in-rotated-sorted-array.go
@@ -1,7 +1,47 @@
 /*
 No33.search-in-rotated-sorted-array
 */
+
+// 2025-12-03 recoding
 func search(nums []int, target int) int {
+	n := len(nums)
+	valK := nums[0]
+
+	l, r, mid := 0, n-1, 0
+	for l <= r {
+		mid = (l + r) / 2
+
+		if nums[mid] >= valK {
+			if nums[mid] < target {
+				l = mid + 1
+			} else if nums[mid] > target {
+				if target >= valK {
+					r = mid - 1
+				} else {
+					l = mid + 1
+				}
+			} else {
+				return mid
+			}
+		} else {
+			if nums[mid] > target {
+				r = mid - 1
+			} else if nums[mid] < target {
+				if target >= valK {
+					r = mid - 1
+				} else {
+					l = mid + 1
+				}
+			} else {
+				return mid
+			}
+		}
+	}
+	return -1
+}
+
+
+func search0(nums []int, target int) int {
 	n := len(nums)
 	l, r := 0, n-1
 	numK := nums[0]
