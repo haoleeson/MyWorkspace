dafa2f47cdc89ef2d7fac07f725454bb4c3d500c
haoleeson <haoleeson@gmail.com>
2025-12-04 00:49:02 +0000

    Recoding(LeetCode): No1.two-sum

--- MyGolang/src/LeetCode/No1.two-sum.go
+++ MyGolang/src/LeetCode/No1.two-sum.go
@@ -1,7 +1,22 @@
 /*
 No1.two-sum
 */
+
+// 2025-12-04 recoding
 func twoSum(nums []int, target int) []int {
+	preNeedMap := make(map[int]int)
+
+	for i, num := range nums {
+		if idx, existed := preNeedMap[num]; existed {
+			return []int{idx, i}
+		} else {
+			preNeedMap[target-num] = i
+		}
+	}
+	return []int{-1, -1}
+}
+
+func twoSum0(nums []int, target int) []int {
 	n := len(nums)
 
 	needNumMap := make(map[int]int)
