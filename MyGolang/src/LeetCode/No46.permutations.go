f19a11721eb5f909d8bbb465920c3b86db5f99d2
haoleeson <haoleeson@gmail.com>
2025-12-05 11:43:53 +0000

    Recoding(LeetCode): No46.permutations

--- MyGolang/src/LeetCode/No46.permutations.go
+++ MyGolang/src/LeetCode/No46.permutations.go
@@ -2,6 +2,37 @@
 No46.permutations
 */
 
+// 2025-12-05 recoding
+func permute(nums []int) [][]int {
+	n := len(nums)
+	ret := make([][]int, 0)
+	path := make([]int, n)
+	vis := make([]bool, n)
+
+	var dfs func(int)
+	dfs = func(idx int) {
+		if idx == n {
+			ret = append(ret, append([]int(nil), path...))
+			return
+		}
+
+		for i := 0; i < n; i++ {
+			if vis[i] {
+				continue
+			}
+
+			vis[i] = true
+			path[idx] = nums[i]
+			dfs(idx + 1)
+			vis[i] = false
+		}
+	}
+
+	dfs(0)
+
+	return ret
+}
+
 func permute(nums []int) [][]int {
 	n := len(nums)
 	path := make([]int, n)
