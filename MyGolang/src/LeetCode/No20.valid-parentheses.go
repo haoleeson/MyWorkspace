d3c055538705d185805fc0ef2a3b4e76a8f919a8
haoleeson <haoleeson@gmail.com>
2025-12-07 01:19:59 +0000

    Recoding(LeetCode): No20.valid-parentheses

--- MyGolang/src/LeetCode/No20.valid-parentheses.go
+++ MyGolang/src/LeetCode/No20.valid-parentheses.go
@@ -1,7 +1,47 @@
 /*
 No20.valid-parentheses
 */
+
+// 2025-12-07 recoding
 func isValid(s string) bool {
+	n := len(s)
+	miniStack := make([]byte, n)
+	size := 0
+
+	for i := 0; i < n; i++ {
+		ch := byte(s[i])
+		switch ch {
+		case '(', '[', '{':
+			// push
+			miniStack[size] = ch
+			size++
+		case ')', ']', '}':
+			if size == 0 {
+				return false
+			}
+			switch ch {
+			case ')':
+				if miniStack[size-1] != '(' {
+					return false
+				}
+			case ']':
+				if miniStack[size-1] != '[' {
+					return false
+				}
+			case '}':
+				if miniStack[size-1] != '{' {
+					return false
+				}
+			}
+			// pop
+			size--
+		}
+	}
+
+	return size == 0
+}
+
+func isValid0(s string) bool {
 	n := len(s)
 	stack := make([]byte, n)
 	sLen := 0
